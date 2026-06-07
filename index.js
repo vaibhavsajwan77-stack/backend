6787f3d02daea96dff7e3aa41f1da381bb13799e
Vaibhav <vaibhavsajwan77@gmail.com>
2026-06-08 02:57:09 +0530

    github data added

--- index.js
+++ index.js
@@ -2,7 +2,36 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const port = 8000;
-
+const githubDATA = {
+    "url": "https://api.github.com/users/vaibhavsajwan",
+    "html_url": "https://github.com/vaibhavsajwan",
+    "followers_url": "https://api.github.com/users/vaibhavsajwan/followers",
+    "following_url": "https://api.github.com/users/vaibhavsajwan/following{/other_user}",
+    "gists_url": "https://api.github.com/users/vaibhavsajwan/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/vaibhavsajwan/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/vaibhavsajwan/subscriptions",
+    "organizations_url": "https://api.github.com/users/vaibhavsajwan/orgs",
+    "repos_url": "https://api.github.com/users/vaibhavsajwan/repos",
+    "events_url": "https://api.github.com/users/vaibhavsajwan/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/vaibhavsajwan/received_events",
+    "type": "User",
+    "user_view_type": "public",
+    "site_admin": false,
+    "name": null,
+    "company": null,
+    "blog": "",
+    "location": null,
+    "email": null,
+    "hireable": null,
+    "bio": null,
+    "twitter_username": null,
+    "public_repos": 2,
+    "public_gists": 0,
+    "followers": 0,
+    "following": 0,
+    "created_at": "2023-07-16T02:12:32Z",
+    "updated_at": "2023-07-18T03:25:13Z"
+}
 app.get('/', (req, res) => {
     res.send('Hello World!');
 
@@ -16,6 +45,9 @@ app.get('/netflix/login', (req, res) => {
 })
 app.get('/youtube', (req, res) => {
     res.redirect('https://www.youtube.com/');
+});
+app.get('/github', (req, res) => {
+    res.json(githubDATA);
 });
 app.listen(process.env.port, () => {
     console.log(`Example app listening on port ${port}`);
